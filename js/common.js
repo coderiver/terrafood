3481e22a886ccb3dbf6d619628676f1604b09e76
akella <akella@Yuris-MacBook-Pro.local>
2013-11-26 00:26:46 +0200

    catalog sidebar

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 
+
+	$('.sproduct__title').click(function(event) {
+		$(this).parent().toggleClass('is-opened')
+	});
+	$('.sproduct__showmore').click(function(event) {
+		$(this).closest('.sproduct').toggleClass('is-showmore')
+	});
 });
